969b5d0601c4cd0f9c278285dd5bceea1892bb98
Pharmow <femcast@yahoo.com>
2023-08-21 11:38:38 +0000

    2nd commit

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     branches: "main",
-    repositoryUrl: "https://github.com/Pharmow/Terra-practice.git",
+    repositoryUrl: "https://github.com/Pharmow/just-a-test.git",
     plugins: [
         '@semantic-release/commit-analyzer',
         '@sematic-release/release-notes-generator',
