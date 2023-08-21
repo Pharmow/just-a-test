9adacdab59fedccb60cc43dea1ca3f8a8caf14ce
Pharmow <femcast@yahoo.com>
2023-08-21 11:40:17 +0000

    2nd commit

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@ module.exports = {
     repositoryUrl: "https://github.com/Pharmow/just-a-test.git",
     plugins: [
         '@semantic-release/commit-analyzer',
-        '@sematic-release/release-notes-generator',
+        '@semantic-release/release-notes-generator',
         '@semantic-release/git',
         '@semantic-release/github'
     ]
